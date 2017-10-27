bfad56b19d5987f63c8ce46a0d734f20eba52dc4
Aleksandra Knezevic <sandra16986@gmail.com>
2017-10-27 12:41:40 -0700

    Update runSQuery.py

--- centralized_scheduler_with_profiler/runSQuery.py
+++ centralized_scheduler_with_profiler/runSQuery.py
@@ -1,3 +1,11 @@
+
+"""
+ ** Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
+ **     contributor: Quynh Nguyen, Bhaskar Krishnamachari
+ **     Read license file in main directory for more details
+"""
+
+
 import os
 import csv
 import pymongo
