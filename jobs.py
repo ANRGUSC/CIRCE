e269efc14275ee6f286e8ec1f2ebd09e21e82dca
Aleksandra Knezevic <sandra16986@gmail.com>
2017-10-27 12:48:37 -0700

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,15 +1,22 @@
+"""
+ ** Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
+ **     contributor: Quynh Nguyen, Bhaskar Krishnamachari
+ **     Read license file in main directory for more details
+"""
+
+
 import paramiko
 import time, os
 
-node1="104.131.111.58"
-node2="139.59.58.153"
-node3="207.154.243.148"
+node1="IP"
+node2="IP"
+node3="IP"
 
 def control_node104():
 
     ssh1 = paramiko.SSHClient()
     ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh1.connect(node1 , username="apac", password="apac20!7")
+    ssh1.connect(node1 , username="user", password="password")
     print "node1 connect successfully!"
     ssh1.exec_command('python2 mongo_script/install_package.py')
     print "dependency install successfully!"
@@ -20,7 +27,7 @@ def control_node139():
 
     ssh2 = paramiko.SSHClient()
     ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh2.connect(node2 , username="apac", password="apac20!7")
+    ssh2.connect(node2 , username="user", password="password")
     print "node2 connect successfully!"
     ssh2.exec_command('python2 mongo_script/install_package.py')
     print "dependency install successfully!"
@@ -31,7 +38,7 @@ def control_node207():
 
     ssh3 = paramiko.SSHClient()
     ssh3.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh3.connect(node2 , username="apac", password="apac20!7")
+    ssh3.connect(node2 , username="user", password="password")
     print "node3 connect successfully!\n"
     ssh3.exec_command('python2 mongo_script/install_package.py')
     print "dependency install successfully!"
